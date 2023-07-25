1014fd721f970977f8a415610f2bdf99caf872e5
David (Pololu) <dev-david@pololu.com>
2023-07-25 09:01:42 -0700

    pico: Got the MS OS 2.0 descriptors working.

--- test/firmware/pico/usb_descriptors.c
+++ test/firmware/pico/usb_descriptors.c
@@ -15,7 +15,7 @@ static const tusb_desc_device_t desc_device =
 {
   .bLength = sizeof(tusb_desc_device_t),
   .bDescriptorType = TUSB_DESC_DEVICE,
-  .bcdUSB = 0x0200,  // TODO: change to 0x0201, use MS OS 2.0 Descriptors
+  .bcdUSB = 0x0201,
 
   .bDeviceClass = TUSB_CLASS_MISC,
   .bDeviceSubClass = MISC_SUBCLASS_COMMON,
@@ -24,7 +24,7 @@ static const tusb_desc_device_t desc_device =
 
   // TODO: after we get the descriptors right, use a real VID/PID here
   .idVendor = 0xCAFE,
-  .idProduct = 0x0008,
+  .idProduct = 0x0009,
   .bcdDevice = 0x0100,
   .iManufacturer = 1,
   .iProduct = 2,
@@ -91,6 +91,96 @@ const uint16_t * tud_descriptor_string_cb(uint8_t index, uint16_t __unused langi
   if (index >= sizeof(strings) / sizeof(strings[0])) { return NULL; }
   return (const void *)strings[index];
 }
+
+//// Microsoft OS 2.0 descriptors and BOS //////////////////////////////////////
+
+// Microsoft OS 2.0 Descriptors, Table 8
+#define MS_OS_20_DESCRIPTOR_INDEX 7
+
+// Microsoft OS 2.0 Descriptors, Table 9
+#define MS_OS_20_FEATURE_COMPATIBLE_ID 0x03
+
+#define REQUEST_GET_MS_DESCRIPTOR 0x20
+
+#define MS_OS_20_LENGTH 0xB2
+
+const uint8_t desc_ms_os_20[] = {
+  // Microsoft OS 2.0 Descriptor Set header (Table 10)
+  0x0A, 0x00,  // wLength
+  MS_OS_20_SET_HEADER_DESCRIPTOR, 0x00,
+  0x00, 0x00, 0x03, 0x06,  // dwWindowsVersion: Windows 8.1 (NTDDI_WINBLUE)
+  MS_OS_20_LENGTH, 0x00,  // wTotalLength
+
+  // Microsoft OS 2.0 configuration subset (Table 11)
+  0x08, 0x00,   // wLength of this header
+  MS_OS_20_SUBSET_HEADER_CONFIGURATION, 0x00,  // wDescriptorType
+  0,            // configuration index
+  0x00,         // bReserved
+  0xA8, 0x00,   // wTotalLength of this subset
+
+  // Microsoft OS 2.0 function subset header (Table 12)
+  0x08, 0x00,  // wLength
+  MS_OS_20_SUBSET_HEADER_FUNCTION, 0x00,  // wDescriptorType
+  0,           // bFirstInterface
+  0x00,        // bReserved,
+  0xA0, 0x00,  // wSubsetLength
+
+  // Microsoft OS 2.0 compatible ID descriptor (Table 13)
+  0x14, 0x00,                                      // wLength
+  MS_OS_20_FEATURE_COMPATIBLE_ID, 0x00,            // wDescriptorType
+  'W', 'I', 'N', 'U', 'S', 'B', 0x00, 0x00,        // compatibleID
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // subCompatibleID
+
+  // Microsoft OS 2.0 registry property descriptor (Table 14)
+  0x84, 0x00,   // wLength
+  MS_OS_20_FEATURE_REG_PROPERTY, 0x00,
+  0x07, 0x00,   // wPropertyDataType: REG_MULTI_SZ
+  0x2a, 0x00,   // wPropertyNameLength
+  'D',0,'e',0,'v',0,'i',0,'c',0,'e',0,'I',0,'n',0,'t',0,'e',0,'r',0,
+  'f',0,'a',0,'c',0,'e',0,'G',0,'U',0,'I',0,'D',0,'s',0,0,0,
+  0x50, 0x00,   // wPropertyDataLength
+  // GUID that represents libusbp test devices (generate your own GUID
+  // if you are copying this example for your own device)
+  '{',0,'9',0,'9',0,'c',0,'4',0,'b',0,'b',0,'b',0,'0',0,'-',0,
+  'e',0,'9',0,'2',0,'5',0,'-',0,'4',0,'3',0,'9',0,'7',0,'-',0,
+  'a',0,'f',0,'e',0,'e',0,'-',0,'9',0,'8',0,'1',0,'c',0,'d',0,
+  '0',0,'7',0,'0',0,'2',0,'1',0,'6',0,'3',0,'}',0,0,0,0,0,
+};
+
+static_assert(MS_OS_20_LENGTH == sizeof(desc_ms_os_20));
+
+#define BOS_TOTAL_LEN (TUD_BOS_DESC_LEN + TUD_BOS_MICROSOFT_OS_DESC_LEN)
+
+const uint8_t desc_bos[] =
+{
+  // total length, number of device caps
+  TUD_BOS_DESCRIPTOR(BOS_TOTAL_LEN, 1),
+
+  TUD_BOS_MS_OS_20_DESCRIPTOR(MS_OS_20_LENGTH, REQUEST_GET_MS_DESCRIPTOR)
+};
+
+static_assert(BOS_TOTAL_LEN == sizeof(desc_bos));
+
+const uint8_t * tud_descriptor_bos_cb()
+{
+  return desc_bos;
+}
+
+bool tud_vendor_control_xfer_cb(uint8_t rhport, uint8_t stage,
+  const tusb_control_request_t * request)
+{
+  if (stage != CONTROL_STAGE_SETUP) { return true; }
+
+  if (request->bmRequestType == 0xC0
+    && request->bRequest == REQUEST_GET_MS_DESCRIPTOR
+    && request->wIndex == MS_OS_20_DESCRIPTOR_INDEX)
+  {
+    return tud_control_xfer(rhport, request, (void *)desc_ms_os_20, sizeof(desc_ms_os_20));
+  }
+
+  return false;
+}
+
 
 //// vendor3: Driver for our vendor-defined interfaces /////////////////////////
 
